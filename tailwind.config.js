40f951e07510767ebdc757e7cb6df52909993eb0
Ahmad Rulim <personal@rulim34.dev>
2023-02-24 07:53:13 +0700

    project: Setup TailwindCSS
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      '2xs': '312px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        'cornflower-blue': '#6f8cf7',
+        'vampire-black': '#050505',
+      },
+    },
+  },
+  plugins: [],
+};
